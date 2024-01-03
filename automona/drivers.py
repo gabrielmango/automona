94081efb8840fa86ae34869daf169d6905be643f
Gabriel Mango <gabrielhmango@gmail.com>
2024-01-03 12:00:49 -0300

    feat: update Productivity

--- automona/drivers.py
+++ automona/drivers.py
@@ -3,6 +3,8 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -11,40 +13,47 @@ class HandleDriver:
         self.background = background
         self.driver = None
 
-    def __create_drive(self):
+    def create_drive(self):
         """Create a instance of selenium webdrive."""
         service = ChromeService(ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
+        options.add_argument(
+            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        )
         if self.background:
             options.add_argument('--headless=new')
             self.driver = webdriver.Chrome(service=service, options=options)
         else:
             self.driver = webdriver.Chrome(service=service)
 
-    def __wait_to_load_element(self, xpath_value, timeout=1):
-        """Wait for an element to be present in the DOM."""
-        while True:
-            sleep(timeout)
-            if self.driver.find_element(By.XPATH, xpath_value):
-                return self.driver.find_element(By.XPATH, xpath_value)
+    def wait_element(self, xpath_value, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, xpath_value))
+        )
+        return self.driver.find_element(By.XPATH, xpath_value)
 
-    def __open_website(self):
+    def load_page(self, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body'))
+        )
+
+    def open_website(self):
         """Open website with url."""
         self.driver.get(self.url)
-        sleep(1)
+        self.load_page()
 
     def start(self, url):
         self.get_url(url)
-        self.__create_drive()
-        self.__open_website()
+        self.create_drive()
+        self.open_website()
 
     def click(self, xpath):
         """Click on selenium webdriver element."""
-        self.__wait_to_load_element(xpath).click()
+        self.wait_element(xpath).click()
 
     def send_keys(self, xpath, keys):
         """Send keys to selenium webdriver element."""
-        self.__wait_to_load_element(xpath).send_keys(keys)
+        self.wait_element(xpath).send_keys(keys)
 
     def get_url(self, value):
         """Set a new url."""
--- automona/tasks.py
+++ automona/tasks.py
@@ -13,7 +13,9 @@ class Login:
         self.password = None
         self.url = None
         self.__get_env()
-        self.start()
+        self.__login()
+        sleep(1)
+        self.handle_driver.load_page()
 
     def __get_env(self):
         config = dotenv_values('.env')
@@ -21,47 +23,69 @@ class Login:
         self.password = config['PASSWORD']
         self.url = config['URL']
 
-    def start(self, _time=3):
+    def __login(self):
         self.handle_driver.start(self.url)
-        self.select_internal_user()
-        self.insert_user()
-        self.insert_password()
-        self.click_login()
-        sleep(_time)
+        self.__select_internal_user()
+        self.__insert_user()
+        self.__insert_password()
+        self.__click_login()
+        self.handle_driver.load_page()
 
-    def select_internal_user(self):
-        self.handle_driver.click('//main/div/div[2]/div/div[1]/div/div[2]')
+    def __select_internal_user(self):
+        self.handle_driver.click(
+            '//main/div/div[2]/div/div[1]/div/div[2]'
+        )
 
-    def insert_user(self):
+    def __insert_user(self):
         self.handle_driver.send_keys(
             '//main/div/div[2]/div/div[3]/form/div/label[1]/div/div[1]/div[2]/input',
             self.user,
         )
 
-    def insert_password(self):
+    def __insert_password(self):
         self.handle_driver.send_keys(
             '//main/div/div[2]/div/div[3]/form/div/label[2]/div/div[1]/div[2]/input',
             self.password,
         )
 
-    def click_login(self):
+    def __click_login(self):
         self.handle_driver.click(
             '//main/div/div[2]/div/div[3]/form/div/div/div[1]/div/button/span[2]'
         )
 
 
-class Productivity:
+class Productivity(Login):
     def __init__(self, handle_driver: Type[HandleDriver]):
-        self.handle_driver = handle_driver()
+        super().__init__(handle_driver)
+        sleep(3)
         self.start()
 
     def start(self):
-        login = Login(HandleDriver)
-        self.click_menu()
+        self.general_menu()
+        self.sti_menu()
+        self.sgp_menu()
+        self.handle_driver.load_page()
+        self.new_productivity()
+        sleep(5)
 
-    def click_menu(self):
+    def general_menu(self):
         self.handle_driver.click(
             '/html/body/div[1]/div/header/div[1]/button[1]'
+        )
+    
+    def sti_menu(self): 
+        self.handle_driver.click(
+            '/html/body/div[3]/div/div[2]/div/nav/ul/li[6]/i'
+        )
+    
+    def sgp_menu(self):
+        self.handle_driver.click(
+            '/html/body/div[4]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[3]/div'
+        )
+    
+    def new_productivity(self):
+        self.handle_driver.click(
+            '/html/body/div[1]/div/div/main/div[1]/div/div[1]/div[2]/div/div/button'
         )
 
 
